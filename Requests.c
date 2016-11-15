310f7aa0629a5114c45dbee9b4f5d98570d91822
vanguard07 <avtansdps@gmail.com>
2016-11-15 13:27:33 +0530

    Bug fixes

--- Requests.c
+++ Requests.c
@@ -1,4 +1,5 @@
 void Requests(char namefromfunction[20])
+ //Checks for friends requests
 {
 		int fdecision;
 		FILE *acceptor;
@@ -20,14 +21,17 @@ void Requests(char namefromfunction[20])
 				count++;
 			}
 			count=count/2;
+			//printf("%d",count);
 			rewind(request);
-			if(count==0)
-				{
-					printf("No friends\n");
-					return;
-				}
+				//printf("%d",count);
 			while(!feof(request))
 			{
+				if(count==0)
+				{
+					printf("No friend requests!\n\n");
+					break;
+					return;
+				}
 				fscanf(request,"%s",temp);
 				printf("%s sent you a friend request?\nDo you want to be his friend?\n1.Yes\n2.No\n",temp);
 				scanf("%d",&fdecision);
@@ -37,25 +41,25 @@ void Requests(char namefromfunction[20])
 					FILE *sender;
 					char S[20];
 					FILE *add;
-					
+
 					strcpy(N,namefromfunction);
 					char *accept=strcat(N,".txt");
-					add=fopen(accept,"a+");	
+					add=fopen(accept,"a+");
 					fprintf(add,"%s\n", temp);
 					fclose(add);
-					
+
 					strcpy(S, temp);
 					char *sent=strcat(S,".txt");
 					sender=fopen(sent, "a+");
 					fprintf(sender, "%s\n",namefromfunction);
-					fclose(sender);		
+					fclose(sender);
 				}
 			count--;
 			if(count==0)
 			break;
 			}
 		fclose(request);
-		printf("%s", check);
 		remove(check);
 		rename("tempr.txt", check);
-	}
+		Exist(namefromfunction);
+}
