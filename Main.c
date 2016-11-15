90eba947c2ca8eb28517ef56bc93501b60010e9f
vanguard07 <avtansdps@gmail.com>
2016-11-15 13:20:58 +0530

    Updated main function.

--- Main.c
+++ Main.c
@@ -7,56 +7,38 @@ struct User
 	};
 
 
-void main()
+int main()
 {
-	char pass[20], user[20], file[30];
-	int n, a;
-	struct User U;
-		char name[20];
-		printf("1.New User\n2.Existing user\nEnter choice: ");	
-		scanf("%d", &a);
+		char username[20], password[20];
+		int x, n, a;
+		struct User U;
+		system("COLOR 60");
+		printf("\t\t******************************Welcome to Postcard********************************************\n");
+		printf("\t\t\t1.New User\n\t\t\t2.Existing user\n\t\t\t3.Quit\n\t\t\tEnter choice: ");
+		//scanf("%d", &a);
 		fflush(stdin);
-		system("cls");
-		if(a==1)
-			{
-			NewLogin(U);
-			//strcpy(name, NewLogin());
-			AddFriends(U.name);
-			}
-		else
-		{
-			printf("\n\tEnter Username or EOF to quit: ");
-			scanf("%s", user);
-			if(strcmp(user, "EOF")!=0)
-				{
-					printf("\n\tEnter Password: ");
-					scanf("%s", pass);
-				}
-				if((ReadUser(user, pass))==1)
-				{
-			do
-			{
-			printf("1.Friend List\n2.Send Message\n3.Logout\n");
-			printf("Enter choice: ");
-			scanf("%d", &n);
-			switch(n)
-			{
-				case 1	: 	strcpy(file, user);
-							char *A = strcat(file, ".txt");
-							Read(A);
-							printf("\n");
-							break;
-				case 2	:	system("cls");
-							//Message();
-							break;
-				case 3	:	system("cls");
-							printf("Logged out successfully!\n");
-							break;
-				default	:	printf("Try again!\n");
-			}
-			}
-			while(n!=3);
-					}
-				}
-}
+		scanf("%d", &a);
+		do
+        {
+            system("cls");
+            switch(a)
+            {
 
+                case 1  :   NewLogin();
+                            break;
+                case 2  :   printf("\n\tEnter Username or EOF to quit: ");
+                            scanf("%s", username);
+                            if(strcmp(username, "EOF")!=0)
+                                {
+                                    printf("\n\tEnter Password: ");
+                                        scanf("%s", password);
+                                }
+                                if((ReadUser(username, password))==1)
+                                    Exist(username);
+                                break;
+                case 3  :   exit(0);
+                default :   main();
+            }
+        }while(a!=3);
+        return 0;
+}
