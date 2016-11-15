0dc962bd19c2dcad22f738605c9ea7d48ca21e3b
vanguard07 <avtansdps@gmail.com>
2016-11-15 13:24:38 +0530

    Updated

--- NewLogin.c
+++ NewLogin.c
@@ -1,25 +1,41 @@
 void NewLogin()
+  //Makes an account of the new user
 {
 	struct User U1;
 	int n;
+	char ch;
 	fflush(stdin);
 	FILE *logptr;
 	if((logptr=fopen("Admin.txt", "a+"))==NULL)
 	printf("File not open");
 	else
 	{
-	printf("Enter Name, Username, Password: ");
-	fscanf(stdin,"%s%s%s", U1.name, U1.username, U1.password);
-	fprintf(logptr, "%s %s %s\n", U1.name, U1.username, U1.password);
-	printf("New account made!\n");
-	printf("\nYour Name is: %s\n Username: %s\n",  U1.name, U1.username);
-	fflush(stdin);
+	    system("cls");
+        printf("\t\t*************************New Login****************************************\n");
+		printf("\n\t\t\tEnter Name: ");
+		fscanf(stdin, "%s", U1.name);
+		printf("\n\t\t\tEnter Username: ");
+		fscanf(stdin, "%s", U1.username);
+		printf("\n\t\t\tEnter Password: ");
+		fscanf(stdin,"%s",U1.password);
+		fprintf(logptr, "%s %s %s\n", U1.name, U1.username, U1.password);
+		printf("\n\t\t\tNew account made!\n");
+		printf("\n\t\t\tYour Name is: %s\n\t\t\tUsername: %s\n\n",  U1.name, U1.username);
+			fflush(stdin);
 	}
-	fclose(logptr);
-	printf("Search Friends?\n1.Yes\n2.No\n");
-	scanf("%d", &n);
-	if(n==1)
-	Search(U1.username);
-	
-}
+	FILE *fri;
+	char *frie=strcat(U1.username,".txt");
+	fri=fopen(frie,"w");
 
+	fclose(fri);
+	fclose(logptr);
+
+	printf("Login again to continue. \n");
+		printf("Press Q to return main menu");
+		scanf("%c",&ch);
+		if (ch=='q' || ch=='Q')
+			 {
+			 	system("cls");
+			 	main();
+	       	 }
+}
